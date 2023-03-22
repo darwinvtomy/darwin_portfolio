c6effc7c673f6d4f8ba576e930b127a5b4e6eff1
DARWIN V TOMY <darwinvtomy@gmail.com>
2023-03-22 20:03:59 +0530

    Changing Caroseal Widget Upgrading the Dependancies

---
+++ lib/utils/horizontal_carouseal.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class HorizontalCarousel extends StatefulWidget {
+  @override
+  _HorizontalCarouselState createState() => _HorizontalCarouselState();
+}
+
+class _HorizontalCarouselState extends State<HorizontalCarousel> {
+  final PageController _pageController = PageController(viewportFraction: 0.3);
+  int _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController.addListener(() {
+      int next = _pageController.page!.round();
+      if (_currentIndex != next) {
+        setState(() {
+          _currentIndex = next;
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final List<Widget> items = [
+      Container(color: Colors.red),
+      Container(color: Colors.green),
+      Container(color: Colors.blue),
+      Container(color: Colors.yellow),
+      Container(color: Colors.purple),
+      Container(color: Colors.orange),
+    ];
+
+    return Column(
+      children: [
+        Expanded(
+          child: PageView.builder(
+            controller: _pageController,
+            itemCount: items.length,
+            itemBuilder: (context, index) {
+              return items[index];
+            },
+            onPageChanged: (int index) {
+              setState(() {
+                _currentIndex = index;
+              });
+            },
+            // Set pageSnapping to false to enable smooth scrolling
+            pageSnapping: true,
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: List<Widget>.generate(items.length, (int index) {
+            return Container(
+              width: 10.0,
+              height: 10.0,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+              decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: _currentIndex == index ? Colors.white : Colors.grey),
+            );
+          }),
+        ),
+      ],
+    );
+  }
+}
